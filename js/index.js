983560f7ffe62bb69a8c67f60e7c6dac030e5fcd
Rat <rat4.email@gmail.com>
2020-11-19 16:49:08 -0500

    ajustes en tamano y eventos

--- js/index.js
+++ js/index.js
@@ -1,87 +1,60 @@
-import { css } from './css';
-var PacificaPopup = /** @class */ (function () {
-    function PacificaPopup(options) {
-        var _this = this;
-        this.options = options;
-        this.overlayStyle = {
-            position: 'absolute',
-            top: '0',
-            lef: '0',
-            bottom: '0',
-            right: '0',
-            left: '0',
-            background: options.overlaycolor || '#2f2f2f6e'
-        };
-        this.popupStyle = {
-            background: 'white',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translateX(-50%) translateY(-50%)',
-            padding: '1em'
-        };
-        this.build();
-        css(this.elements.overlay, this.options.overlayCss.default || {});
-        css(this.elements.popup, this.options.popupCss.default || {});
-        this.applyMediaQuerys();
-        window.addEventListener('resize', function () {
-            _this.applyMediaQuerys();
-        });
-        if (this.options.open) {
-            this.open();
-        }
-        if (this.options.closeOnClickOf) {
-            this.elements.overlay.querySelector(this.options.closeOnClickOf)
-                .addEventListener('click', function () {
-                _this.close();
-            });
-        }
-        window.onscroll = function () {
-            _this.isOpen ? window.scrollTo(0, 0) : null;
-        };
+import { PacificaPopup } from "./popup";
+var popup;
+var keyname = "popupForm";
+window.addEventListener('message', function (event) {
+    var status = event.data;
+    switch (status) {
+        case "submit":
+            popup.close();
+            sessionStorage.setItem(keyname, "true");
+            break;
+        case "error":
+            break;
+        case "invalid":
+            break;
+        case "notAgain":
+            localStorage.setItem(keyname, "true");
+            break;
+        case "cancelNotAgain":
+            localStorage.removeItem(keyname);
+            break;
+        default:
+            break;
     }
-    PacificaPopup.prototype.bindCloseTo = function (element) {
-        var _this = this;
-        element.addEventListener('click', function () {
-            _this.close();
-        });
-    };
-    PacificaPopup.prototype.open = function () {
-        this.elements.overlay.append(this.elements.popup);
-        document.querySelector('body').append(this.elements.overlay);
-        this.isOpen = true;
-    };
-    PacificaPopup.prototype.close = function () {
-        this.elements.overlay.remove();
-        this.isOpen = false;
-    };
-    PacificaPopup.prototype.applyMediaQuerys = function () {
-        if (this.options.overlayCss.mobile && this.options.overlayCss.desktop) {
-            if (window.innerWidth > 768) {
-                css(this.elements.overlay, this.options.overlayCss.desktop || {});
-            }
-            else if (window.innerWidth < 768) {
-                css(this.elements.overlay, this.options.overlayCss.mobile || {});
-            }
-        }
-        if (this.options.popupCss.mobile && this.options.popupCss.desktop) {
-            if (window.innerWidth > 768) {
-                css(this.elements.popup, this.options.popupCss.desktop || {});
-            }
-            else if (window.innerWidth < 768) {
-                css(this.elements.popup, this.options.popupCss.mobile || {});
-            }
-        }
-    };
-    PacificaPopup.prototype.build = function () {
-        var overlay = document.createElement('DIV');
-        var popup = document.createElement('DIV');
-        popup.innerHTML = this.options.html;
-        css(overlay, this.overlayStyle);
-        css(popup, this.popupStyle);
-        this.elements = { overlay: overlay, popup: popup };
-    };
-    return PacificaPopup;
-}());
-window.PacificaPopup = PacificaPopup;
+});
+function injectForm(injectSite, popupPath) {
+    if (popupPath && window.location.pathname != popupPath) {
+        return false;
+    }
+    if (sessionStorage.getItem(keyname)) {
+        return false;
+    }
+    if (localStorage.getItem(keyname)) {
+        return false;
+    }
+    popup = new PacificaPopup({
+        html: "<iframe src=" + injectSite + " height=100% width=100% style=\"border:none; background:white;\"></iframe>\n\t\t<span style=\"position:absolute;top:1.5rem; font-weight:500;font-size:2.5rem;color:#00000078;cursor:pointer;right:2rem;background:#ffffffa1;padding:0.2rem 1rem; border-radius:50px;\">X</span>\n\t",
+        open: true,
+        closeOnClickOf: "span",
+        popupCss: {
+            default: {
+                background: "transparent",
+                textAlign: "center",
+                height: '85vh',
+                width: '100%',
+                maxWidth: "1100px",
+                zIndex: "5000000000",
+                top: '50vh'
+            },
+        },
+        overlayCss: {
+            default: {
+                background: "#000000a6"
+            },
+            desktop: {},
+            mobile: {}
+        },
+    });
+}
+top.paciform = injectForm;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,130 +1,60 @@
-import { css } from './css'
+import { PacificaPopup } from "./popup";
 
-interface IpopupOptions{
-	open:boolean;
-	closeOnClickOf:string;
-	html:string;
-	overlaycolor:string;
-	overlayCss?:any;
-	popupCss?:any;
+var popup:PacificaPopup;
+var keyname="popupForm"
+
+window.addEventListener('message', (event) => {
+	var status=event.data
+	switch (status) {
+		case "submit":
+			popup.close()
+			sessionStorage.setItem(keyname,"true");
+			break;
+		case "error":
+			break;
+		case "invalid":
+			break;
+		case "notAgain":
+			localStorage.setItem(keyname,"true");
+			break;
+		case "cancelNotAgain":
+			localStorage.removeItem(keyname);
+			break;
+		default:
+			break;
+	}
+})
+
+
+function injectForm(injectSite:string,popupPath:string){
+	if(popupPath && window.location.pathname != popupPath){return false}
+	if(sessionStorage.getItem(keyname)){return false}
+	if(localStorage.getItem(keyname)){return false}
+	popup=new PacificaPopup({
+	html:`<iframe src=${injectSite} height=100% width=100% style="border:none; background:white;"></iframe>
+		<span style="position:absolute;top:1.5rem; font-weight:500;font-size:2.5rem;color:#00000078;cursor:pointer;right:2rem;background:#ffffffa1;padding:0.2rem 1rem; border-radius:50px;">X</span>
+	`,
+		open:true,
+		closeOnClickOf:"span",
+		popupCss:{
+			default:{
+				background:"transparent",
+				textAlign:"center",
+				height:'85vh',
+				width:'100%',
+				maxWidth:"1100px",
+				zIndex:"5000000000",
+				top:'50vh'
+			},
+		},
+		overlayCss:{
+			default:{
+				background:"#000000a6"
+			},
+			desktop:{},
+			mobile:{}
+		},
+	})
 }
 
-class PacificaPopup{
-
-	private overlayStyle:any;
-	private popupStyle:any;
-	private elements:any;
-	private isOpen:boolean;
-
-	public closeObject:HTMLElement
-
-	constructor(public options:IpopupOptions){
-
-		this.overlayStyle={
-			position:'absolute',
-			top:'0',
-			lef:'0',
-			bottom:'0',
-			right:'0',
-			left:'0',
-			background:options.overlaycolor || '#2f2f2f6e'
-		}
-		this.popupStyle={
-			background:'white',
-        	position: 'absolute',
-        	top: '50%',
-        	left: '50%',
-        	transform: 'translateX(-50%) translateY(-50%)',
-			padding:'1em'
-		}
-		this.build()
-
-		css(this.elements.overlay,this.options.overlayCss.default || {})
-		css(this.elements.popup,this.options.popupCss.default || {})
-		this.applyMediaQuerys()
-
-		window.addEventListener('resize',()=>{
-
-			this.applyMediaQuerys()
-		})
-
-		if(this.options.open){
-
-			this.open()
-		}
-
-		if(this.options.closeOnClickOf){
-
-			this.elements.overlay.querySelector(this.options.closeOnClickOf)
-				.addEventListener('click',()=>{
-					this.close()
-				})
-		}
-
-		window.onscroll =  ()=> {
-			this.isOpen ? window.scrollTo(0, 0) : null
-		};
-	}
-
-	bindCloseTo(element:HTMLElement){
-		element.addEventListener('click',()=>{
-			this.close()
-		})
-	}
-
-	open(){
-
-		this.elements.overlay.append(this.elements.popup)
-		document.querySelector('body').append(this.elements.overlay)
-		this.isOpen=true
-	}
-
-	close(){
-
-		this.elements.overlay.remove()
-		this.isOpen=false
-	}
-
-	private applyMediaQuerys(){
-
-		if(this.options.overlayCss.mobile && this.options.overlayCss.desktop){
-
-			if(window.innerWidth > 768){
-
-				css(this.elements.overlay,this.options.overlayCss.desktop || {})
-
-			}else if (window.innerWidth < 768){
-
-				css(this.elements.overlay,this.options.overlayCss.mobile || {})
-			}
-		}
-
-		if(this.options.popupCss.mobile && this.options.popupCss.desktop){
-
-			if(window.innerWidth > 768){
-
-				css(this.elements.popup,this.options.popupCss.desktop || {})
-
-			}else if (window.innerWidth < 768){
-
-				css(this.elements.popup,this.options.popupCss.mobile || {})
-			}
-		}
-
-
-	}
-
-
-	private build(){
-
-		var overlay=document.createElement('DIV')
-		var popup=document.createElement('DIV')
-		popup.innerHTML=this.options.html
-
-		css(overlay,this.overlayStyle)
-		css(popup,this.popupStyle)
-	 	this.elements={overlay:overlay,popup:popup}
-	}
-}
-
-window.PacificaPopup=PacificaPopup
+top.paciform=injectForm
